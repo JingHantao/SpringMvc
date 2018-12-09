3ebe1ffb1ae8b8a8985a15a0c7b6e9ea4fdec0ba
Lost97 <1175049698@qq.com>
2018-12-09 22:31:20 +0800

    2018-12-9

--- src/com/atmoon/base/springmvc/controller/HelloController.java
+++ src/com/atmoon/base/springmvc/controller/HelloController.java
@@ -1,9 +1,23 @@
 package com.atmoon.base.springmvc.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
+ * @author asuspc
  * @auther: asuspc
  * @Date: 2018/12/9 15:59
  * @Description:
  */
+@Controller
+@RequestMapping(value = "/hello")
 public class HelloController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String printHello(ModelMap model){
+        model.addAttribute("message","Hello Spring Mvc Framework!");
+        return "hello";
+    }
 }
